2980f3401c43a196827334a217fbdaabd4e64604
gopal-sakshi <gopal612@gmail.com>
2022-10-25 17:52:58 +0530

    changed port

--- heroes.js
+++ heroes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const port = process.argv.slice(2)[0] || 8000;
+const port = process.argv.slice(2)[0] || 3014;
 const app = express();
 app.use(bodyParser.json());
 
